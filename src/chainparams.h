cd83753caba1c2c3d5997da39a4345c2172a6146
Alx <lomtax.ladamberti@gmail.com>
2021-04-25 22:46:56 +0200

    chainparams : add ProofOfWorkLimit(int algo)

--- src/chainparams.h
+++ src/chainparams.h
@@ -73,6 +73,7 @@ public:
     const std::vector<unsigned char>& AlertKey() const { return vAlertPubKey; }
     int GetDefaultPort() const { return nDefaultPort; }
 
+    const CBigNum ProofOfWorkLimit(int algo) const { return consensus.bnProofOfWorkLimit[algo]; }
     const CBlock& GenesisBlock() const { return genesis; }
     const CBlock& DevNetGenesisBlock() const { return devnetGenesis; }
     /** Make miner wait to have peers to avoid wasting work */
